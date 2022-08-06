3e780ecbad9dbe059fdca0378fb5de87b136b909
Lewis3758 <110722206+Lewis3758@users.noreply.github.com>
2022-08-06 21:28:50 +0900

    Add files via upload

---
+++ Calculator4_iOS-master/Calculator5/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Calculator5
+//
+//  Created by LG on 2020/09/09.
+//  Copyright © 2020 LG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping: Bool = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+        }
+        if digit != "0" {
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathmaticalSymbol = sender.currentTitle {
+            brain.performOperator(symbol: mathmaticalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
+
